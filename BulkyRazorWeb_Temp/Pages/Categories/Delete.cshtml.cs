9b995df21d57e170478c22004af17150a18a4b96
Parikshit <phooda804@live.com>
2024-04-08 01:08:23 +0530

    add ctor

--- BulkyRazorWeb_Temp/Pages/Categories/Delete.cshtml.cs
+++ BulkyRazorWeb_Temp/Pages/Categories/Delete.cshtml.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BulkyRazorWeb_Temp.Data;
 
 namespace BulkyRazorWeb_Temp.Pages.Categories
 {
 	public class DeleteModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
         }
--- BulkyRazorWeb_Temp/Pages/Categories/Edit.cshtml.cs
+++ BulkyRazorWeb_Temp/Pages/Categories/Edit.cshtml.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BulkyRazorWeb_Temp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyRazorWeb_Temp.Pages.Categories
 {
 	public class EditModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
         }
