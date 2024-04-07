ee6b050ff13ecaacd6b25d28a944cfa877514b98
Parikshit <phooda804@live.com>
2024-04-07 23:19:01 +0530

    update Program.cs with sqlite service

--- BulkyRazorWeb_Temp/Program.cs
+++ BulkyRazorWeb_Temp/Program.cs
@@ -1,7 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using BulkyRazorWeb_Temp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
